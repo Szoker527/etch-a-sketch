a1d27658d19e76511111c6a10de8117593714e66
szoker <szoker527@gmail.com>
2021-10-22 15:32:56 +0200

    Added all basic files and started 16x16 grid box

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+
+
+for (let i = 0; i < 256; i++) {
+        const divs = document.createElement("div");
+        const container = document.querySelector("#container");
+        container.appendChild(divs);
+    }
+
