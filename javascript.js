8321fe152419a0e550cbe472c276274213e5543a
szoker <szoker527@gmail.com>
2021-10-29 15:25:13 +0200

    Made 16x16 grid container

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
 
 
+function grid(){
 
+const container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 16; i++) {
         const divs = document.createElement("div");
-        const container = document.querySelector("#container");
+        divs.classList.add("divContainer")
         container.appendChild(divs);
+    for (let i = 0; i < 16; i++) {
+        const newDivs = document.createElement("div");
+        newDivs.classList.add("newdivContainer");
+        divs.appendChild(newDivs);
     }
+}
 
+
+}
+
+
+console.log(grid());
