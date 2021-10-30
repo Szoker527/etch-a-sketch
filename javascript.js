bed014e6ad76ee9d6fb9225b73a9a9135c4e0a6e
szoker <szoker527@gmail.com>
2021-10-30 17:34:58 +0200

    added event listiner

--- javascript.js
+++ javascript.js
@@ -1,22 +1,30 @@
-
-
 function grid(){
-
-const container = document.querySelector("#container");
-
-for (let i = 0; i < 16; i++) {
-        const divs = document.createElement("div");
-        divs.classList.add("divContainer")
-        container.appendChild(divs);
+    const container = document.querySelector("#container");
+    
     for (let i = 0; i < 16; i++) {
-        const newDivs = document.createElement("div");
-        newDivs.classList.add("newdivContainer");
-        divs.appendChild(newDivs);
+            const divs = document.createElement("div");
+            divs.classList.add("divContainer")
+            container.appendChild(divs);
+        for (let i = 0; i < 16; i++) {
+            const newDivs = document.createElement("div");
+            newDivs.classList.add("newdivContainer");
+            divs.appendChild(newDivs);
+        }
     }
-}
-
-
-}
-
-
+    
+    
+    }
 console.log(grid());
+
+const multiDivs = document.querySelectorAll("#container")
+
+multiDivs.forEach(div => div.addEventListener("mouseover", point(this)));
+
+
+function point(x) {
+    x.style.color = 'blue';
+}
+
+
+
+
