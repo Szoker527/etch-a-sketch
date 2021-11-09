00165b854859ed5198ed5c1989bb263c55a3695d
szoker <szoker527@gmail.com>
2021-11-09 14:13:22 +0100

    Different colors to mouseover and added 3 buttons

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 function grid(){
     const container = document.querySelector("#container");
     
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 64; i++) {
             const divs = document.createElement("div");
             divs.classList.add("divContainer")
             container.appendChild(divs);
-        for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < 64; i++) {
             const newDivs = document.createElement("div");
             newDivs.classList.add("newdivContainer");
             divs.appendChild(newDivs);
@@ -19,7 +19,8 @@ console.log(grid());
 const multiDivs = document.querySelectorAll(".newdivContainer")
 
 multiDivs.forEach(div => div.addEventListener('mouseover', function (e) {
-    e.target.style.background = 'blue';
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.background =  "#" + randomColor;
   }));
     
     
