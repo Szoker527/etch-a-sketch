daadf25cd05336470bed66ee0a759ea8fa22d233
szoker <szoker527@gmail.com>
2021-11-12 15:23:18 +0100

    Added clear and erase function

--- javascript.js
+++ javascript.js
@@ -2,14 +2,30 @@
 
 const button = document.querySelector("#myBtn")
 
-
-
 button.addEventListener("click", newGrid
     // location.reload();
   )
 
+for (let i = 0; i < 16; i++) {
+    const divs = document.createElement("div");
+    divs.classList.add("divContainer")
+    container.appendChild(divs);
+for (let i = 0; i < 16; i++) {
+    const newDivs = document.createElement("div");
+    newDivs.classList.add("newdivContainer");
+    divs.appendChild(newDivs);
+}
+}
+function colorGrid() {
+const multiDivs = document.querySelectorAll(".newdivContainer")
 
+multiDivs.forEach(div => div.addEventListener('mouseover', function (e) {
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+e.target.style.background =  "#" + randomColor;
+}));
 
+}
+colorGrid()
 
 function grid(gridNumber){
     const container = document.querySelector("#container");
@@ -34,30 +50,67 @@ function grid(gridNumber){
 }
 
 function newGrid() {
-    const text = prompt("How big you want your grid?", "grid number")
+    const text = prompt("How big you want your grid?", "" )
+
+    if (text === "null" || text === null || text === "" ){
+         return;
+    }
+
     let gridNumber = text;
     if (gridNumber < 100) {
-                clearGrid();
+        eraseGrid();
          return grid(gridNumber);
 
     }
-    else {
+    else if(gridNumber > 100) {
+        alert("Grid can't be bigger then 100!")
         return newGrid();
     }
 }  
 
-function clearGrid() {
+function eraseGrid() {
     const myNode = document.getElementById("container");
     while (myNode.firstChild) {
       myNode.removeChild(myNode.lastChild);
     }
 }
 
-button.addEventListener("click", () => {
-    const container = document.querySelectorAll(".divContainer");
-    console.log(container.length);
-}
-)
+function resetGrid() {
+    eraseGrid();
+
+    for (let i = 0; i < 16; i++) {
+        const divs = document.createElement("div");
+        divs.classList.add("divContainer")
+        container.appendChild(divs);
+    for (let i = 0; i < 16; i++) {
+        const newDivs = document.createElement("div");
+        newDivs.classList.add("newdivContainer");
+        divs.appendChild(newDivs);
+    }
+    }
+    const multiDivs = document.querySelectorAll(".newdivContainer")
     
+    multiDivs.forEach(div => div.addEventListener('mouseover', function (e) {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.background =  "#" + randomColor;
+    }));
+}
+
+function clearGrid() {
+    const multiDivs = document.querySelectorAll(".newdivContainer")
+    
+    multiDivs.forEach(div => div.addEventListener('mouseover', function (e) {
+        e.target.style.background =  "#0000" ;
+  }));
+} 
+    
+const btnReset = document.querySelector("#gridGone")
+
+btnReset.addEventListener("click", resetGrid) 
+
+const btnClear = document.querySelector("#clearGrid")
+
+btnClear.addEventListener("click", clearGrid) 
+
 
 
